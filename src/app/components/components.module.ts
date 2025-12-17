cffa3dbc9fd9ca835ad0e5d6b432d111ce60eaa4
thiago_osim <thiago_osim@hotmail.com>
2025-12-17 20:36:43 -0300

    Criado o componente filter.component para criação do layout do filtro e l ógica

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from "@angular/core";
 import { AngularMaterialModule } from "../angular-material/angular-material.module";
 import { UserDetailsComponent } from './user-details/user-details.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
-    UserDetailsComponent
+    UserDetailsComponent,
+    FilterComponent
   ],
   imports: [
     AngularMaterialModule
   ],
   exports: [
-    UserDetailsComponent
+    UserDetailsComponent,
+    FilterComponent
   ]
 })
 export class ComponentsModule {}
