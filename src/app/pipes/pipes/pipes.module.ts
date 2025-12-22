88276846e6b8d1905126ed77d5c3f1828a3750ea
thiago_osim <thiago_osim@hotmail.com>
2025-12-22 19:50:43 -0300

    Criado pipe para endereço

--- src/app/pipes/pipes/pipes.module.ts
+++ src/app/pipes/pipes/pipes.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhonePipe } from './phone.pipe';
+import { AddressPipe } from './address.pipe';
 
 
 
 @NgModule({
   declarations: [
-    PhonePipe
+    PhonePipe,
+    AddressPipe
   ],
   imports: [
     CommonModule
