6f8623fabd2a29f0dd6152d7528d5deb9085ab9a
thiago_osim <thiago_osim@hotmail.com>
2025-12-22 20:04:20 -0300

    Criado pipe status

--- src/app/pipes/pipes/pipes.module.ts
+++ src/app/pipes/pipes/pipes.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhonePipe } from './phone.pipe';
 import { AddressPipe } from './address.pipe';
+import { StatusPipe } from './status.pipe';
 
 
 
 @NgModule({
   declarations: [
     PhonePipe,
-    AddressPipe
+    AddressPipe,
+    StatusPipe
   ],
   imports: [
     CommonModule
